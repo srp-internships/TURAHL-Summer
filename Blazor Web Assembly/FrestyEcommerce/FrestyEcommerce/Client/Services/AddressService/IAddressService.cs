be1a7f4b4f14134a05ff47af5ee21dda722bc0ce
Ahliddinkhon <66516355+8ESTIE@users.noreply.github.com>
2023-12-17 21:23:56 +0500

    Delete Blazor Web Assembly/FrestyEcommerce directory

--- Blazor Web Assembly/FrestyEcommerce/FrestyEcommerce/Client/Services/AddressService/IAddressService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FrestyEcommerce.Client.Services.AddressService
-{
-    public interface IAddressService
-    {
-        Task<Address> GetAddress();
-        Task<Address> AddOrUpdateAddress(Address address);
-    }
-}
--- Blazor Web Assembly/FrestyEcommerce/FrestyEcommerce/Server/Services/ProductTypeService/IProductTypeService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FrestyEcommerce.Server.Services.ProductTypeService
-{
-    public interface IProductTypeService
-    {
-        Task<ServiceResponse<List<ProductType>>> GetProductTypes();
-        Task<ServiceResponse<List<ProductType>>> AddProductType(ProductType productType);
-        Task<ServiceResponse<List<ProductType>>> UpdateProductType(ProductType productType);
-    }
-}
